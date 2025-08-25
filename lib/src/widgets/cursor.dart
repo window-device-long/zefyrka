4762294dd0e585e4b0183af9795581380c1f42a2
window-device-long <longnpdevmobile@gmail.com>
2025-08-25 11:04:21 +0700

    Update cursor.dart

--- lib/src/widgets/cursor.dart
+++ lib/src/widgets/cursor.dart
@@ -97,7 +97,7 @@ class CursorStyle {
   }
 
   @override
-  int get hashCode => hashValues(color, backgroundColor, width, height, radius,
+  int get hashCode => Object.hash(color, backgroundColor, width, height, radius,
       offset, opacityAnimates, paintAboveText);
 }
 
